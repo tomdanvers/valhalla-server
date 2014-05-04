acca899e7403c79672037194411c2e3aafbfed00
Tom Danvers <tom@tomdanvers.com>
2014-05-04 19:12:55 +0100

    Fixed start shell script.

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./node_modules/forever/bin/forever start -al forever.log -ao out.log -ae err.log valhalla-svalhalla-svalhalla-svalhalla-svalhalla-svalhalla-svalhalla-svalhalla-server.js
+./node_modules/forever/bin/forever start -al forever.log -ao out.log -ae err.log valhalla-server.js
