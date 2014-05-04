299398ec10b94fbc6625918e32a388a3b1777b32
Tom Danvers <tom@tomdanvers.com>
2014-05-04 19:25:05 +0100

    Testing CORS headers.

--- valhalla-server.js
+++ valhalla-server.js
@@ -24,8 +24,19 @@ function route(request, response) {
 	switch(request.url){
 		case '/settings.json':
 			response.header('Access-Control-Allow-Origin', '*');
-			response.header('Access-Control-Allow-Headers', 'X-Requested-With');
-			response.send(JSON.stringify(Settings));
+		    	response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+		    	response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+		    	// intercept OPTIONS method
+		    	if ('OPTIONS' == request.method) {
+		     	response.send(200);
+		    	}
+		    	else {
+		     	response.send(JSON.stringify(Settings));
+		    	}
+
+			//response.header('Access-Control-Allow-Origin', '*');
+			//response.header('Access-Control-Allow-Headers', 'X-Requested-With');
 			break;
 		default:
 			response.redirect(environment.client);
