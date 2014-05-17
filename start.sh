90d4388b82a275a05a21b97fefd454606efe8892
Tom Danvers <tom@tomdanvers.com>
2014-05-17 10:50:28 +0100

    Added pull shell script.

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+./node_modules/forever/bin/forever stop -al forever.log -ao out.log -ae err.log valhalla-server.js
+git pull origin master
 ./node_modules/forever/bin/forever start -al forever.log -ao out.log -ae err.log valhalla-server.js
